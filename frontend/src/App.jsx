d8a13a9344d0e32d4846989685c8d68c53c66d25
An-Jie Yu <anjieyu9@gmail.com>
2026-07-16 18:04:55 -0700

    add the notes component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,16 @@
 import {render} from 'solid-js/web';
 import {createSignal, onMount} from 'solid-js';
+import {Notes} from './layouts/Notes';
 import './style.css';
  
 function App() {
 
     return (
-        <div>
+        <div class='app'>
             <h1>My Notes App</h1>
             <div class='app-container'>
                 <p>Here is the notes app</p>
+                <Notes/>
             </div>
         </div>
     );
---
+++ frontend/src/layouts/Notes.jsx
@@ -0,0 +1,9 @@
+import {createSignal} from 'solid-js';
+
+export function Notes(){
+    return (
+        <section class='notes'>
+            <p>This is where the notes will go.</p>
+        </section>
+    );
+}
